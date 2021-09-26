49c130a5fb22798e86b04e030c460a27185f5363
ArtyomLe <art.lv@hotmail.com>
2021-09-26 21:16:34 +0300

    Максимальное и минимальное значение из двумерного списка

--- MaxMin.py
+++ MaxMin.py
@@ -7,3 +7,14 @@ for i in range(line):
     for n in range(colm):
         a[i].append(random.randint(10, 99))
 
+max_value = a[0][0]
+min_value = a[0][0]
+
+for i in range(line):
+    for j in range(colm):
+        if(a[i][j] > max_value):
+            max_value = a[i][j]
+        if(a[i][j] < min_value):
+            min_value = a[i][j]
+print(f"Максимальное значение: {max_value}")
+print(f"Минимальное значение: {min_value}")
