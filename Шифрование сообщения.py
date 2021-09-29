d9bc25fd1a5eaa7f3036b04812a189a42711c6b7
ArtyomLe <art.lv@hotmail.com>
2021-09-29 23:09:58 +0300

    Результат шифрования можно просмотреть в файле (дешифрование сообщения)

---
+++ Шифрование сообщения.py
@@ -0,0 +1,16 @@
+import random
+toCode = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
+sIn = input("Введите строку для шифрования: ").upper()
+sOut = ""
+
+for i in range(len(sIn)):
+    count = random.randint(0, 10)
+    for j in range(count):
+        sOut += toCode[random.randint(0, len(toCode) - 1)]
+    if (random.randint(0, 1) == 1):
+        sOut += "А"
+    else:
+        sOut += "Б"
+    sOut += sIn[i]
+print(f"Зашифрованная строка: {sOut}")
+
