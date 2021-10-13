cc0085b7b43a984087ba099dea27d7d495e2edf6
Artyom Levykh <art.lv@hotmail.com>
2021-10-13 22:08:16 +0300

    НАИБОЛЬШИЙ ОБЩИЙ ДЕЛИТЕЛЬ(Функция)

--- Recursion.py
+++ Recursion.py
@@ -23,13 +23,25 @@ def req(n):
         print(n)
 req(0)
 '''
-# NOD => НАИБОЛЬШИЙ ОБЩИЙ ДЕЛИТЕЛЬ
+# NOD => НАИБОЛЬШИЙ ОБЩИЙ ДЕЛИТЕЛЬ(Функция)
 def nod(a, b):
-    while(a != 0 and b != 0):
-        if(a > b):
-            a %= b
-        else:
-            b %= a
-    return a + b
+    while(a != 0 and b != 0): # 75   and   30 | 15   and   30 | 15   and   0  |               |
+        if(a > b):            #               |               |               |               |
+            a %= b            # a = 75%30(15) |               |               |               |
+        else:                 #               |               |               |               |
+            b %= a            #               | b = 30%15(0)  |               |               |
+    return a + b              #               |               |return 15+0(15)|               |
 
-print(nod(7, 13))                  # Вызываем функию НОД и печатаем её результат
+print(nod(75, 30))                  # Вызываем функию НОД и печатаем её результат
+
+
+# NOD => НАИБОЛЬШИЙ ОБЩИЙ ДЕЛИТЕЛЬ(Рекурсия)
+def nod(a, b):
+    if(a == 0 or b == 0):
+        return a + b
+    if (a > b):
+        return nod(a % b, b)
+    else:
+        return nod(a, b % a)
+
+print(nod(75, 30))
