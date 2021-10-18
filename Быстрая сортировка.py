f9c1b477a587fcdb86a3b5b87628645751ce8dd8
Artyom Levykh <art.lv@hotmail.com>
2021-10-18 21:44:16 +0300

    Для экономии ресурсов процессора, можно не высчитывать каждый раз среднее арифметическое

--- Быстрая сортировка.py
+++ Быстрая сортировка.py
@@ -44,3 +44,33 @@ print(a)
    кроме списка равных среднему арифметическому элементов.
 
 """
+
+# Для экономии ресурсов процессора, можно не высчитывать каждый раз среднее арифметическое
+
+def qsort(a):
+    if (len(a) == 2):
+        if (a[0] > a[1]):
+            a[0], a[1] = a[1], a[0]
+        return a
+    elif (len(a) > 2):
+        average = a[len(a) // 2] # просто берём элемент по центру списка
+        print(average)
+
+        sp1 = []                 # Элементы меньше среднего
+        sp2 = []                 # Элементы равные среднему
+        sp3 = []                 # Элементы больше среднего
+
+        for i in a:
+            if (i < average):
+                sp1.append(i)
+            elif (i > average):
+                sp3.append(i)
+            else:
+                sp2.append(i)
+        return qsort(sp1) + sp2 + qsort(sp3)
+    return a
+
+a = [5, 8, 1, 5, 3, 5, 2, 0, 5, 2]
+print(a)
+a = qsort(a)
+print(a)
