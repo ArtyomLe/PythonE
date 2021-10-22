437901120508f528e7be88857c42efb611d1f20f
Artyom Levykh <art.lv@hotmail.com>
2021-10-22 20:27:02 +0300

    Перевод в различные системы счисления

---
+++ Длинная арифметика2.py
@@ -0,0 +1,16 @@
+# Перевод в различные системы счисления
+def getData(a, osnovsnie):
+    d = 0
+    length = len(a)
+    for i in range(length):
+        d += a[i] * osnovsnie ** i # ФОРМУЛА:   < Цифра числа * Основание^ Позиция цифры >
+    return d
+
+# def printData(a):
+#     for i in range(len(a) - 1, -1, -1):
+#         print(f" {a[i]}", end="")
+#     print()
+
+# a = [3, 2, 1]
+a = [0, 0, 0, 0, 1]
+print(getData(a, 16))
