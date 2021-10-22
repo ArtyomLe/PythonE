16a16d39a1b3d4a4244987ca2ab426b67fb9a4f3
Artyom Levykh <art.lv@hotmail.com>
2021-10-22 17:41:05 +0300

    По разному зашифровываем одно и тоже число

--- Длинная арифметика1.py
+++ Длинная арифметика1.py
@@ -1,13 +1,24 @@
+# По разному зашифровываем одно и тоже число
 
 def getData(a):
     d = 0
     length = len(a)
-
     for i in range(length):
         d += a[i] * 10 ** i
     return d
 
-a = [54, 21, 67, 12, 54]
-print(getData(a))
+def printData(a):
+    for i in range(len(a) - 1, -1, -1):
+        print(f" {a[i]}", end="")
+    print()
+
+print("Обычное число")
 a = [4, 6, 9, 8, 5, 5]
+printData(a)
 print(getData(a))
+
+print("\nЗашифрованное число")
+a = [54, 21, 67, 12, 54]     # 54 + 210 + 6700 + 12000 + 540000
+printData(a)
+print(getData(a))
+
