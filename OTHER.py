987b8e63fcd0c09aefdb85cfbf09010251cabf73
Artyom Levykh <art.lv@hotmail.com>
2021-10-25 19:57:24 +0300

    Check yourself

--- OTHER.py
+++ OTHER.py
@@ -21,3 +21,16 @@ for i in range(5):
         if (b[i][j] % 5 != 0):
             s += 1
 print(s)
+print("\n")
+
+# ++++++++++++++++++++++++++++++
+
+def f(w):
+    s = 0
+    for i in range(len(w)):     # len(w) = 4     =>  ((1, 4) - (5, 1))
+        s += w[i]               # s = 0+1(1) => s = 1+4(5) => s = 5 -5(0) => 0 -1(-1)  *** f(h)=5   -   f(p)=-6 ***
+#        print(w[i])
+    return s
+h = [1, 4]
+p = [5, 1]
+print(f(h) - f(p))
